afdf6b2bb700a79c14416c70969fa9904c01147c
taylulz <taylulzcode@gmail.com>
2020-11-06 08:44:59 -0800

    passes test spec to create new Pizza objects

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+function Pizza(size, toppings) {
+  this.size = size;
+  this.toppings = toppings;
+}
+
+
+
+
+// | Create pizza objects with constructor |||
+// | should correctly create a new Pizza object | pizza1 | Pizza {size, [toppings]} |
+
+// | Cost of pizza prototype |||
+// | should correctly add number of toppings | 3 | pizza1.cost() returns 3 |
+// | should correctly add price of sm, med, lrg | med | 10 |
+
+// | Display final cost to user |||
+// | should correctly total the size + amount of toppings | sm and 3 toppings | display 8 |
+
+
+// <!-- Create a pizza object constructor: let pizza1 = new Pizza ([toppings], size) {[anchovies, olives, artichokes], small} -->
